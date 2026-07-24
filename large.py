89965bc1792a8787cedcbc14e7df22e5d2a4706a
sreenanda <sreenandavh@gmail.com>
2026-07-24 13:47:41 +0530

    Finally, got the second largest

--- large.py
+++ large.py
@@ -2,9 +2,13 @@
 #if I want the sec largest number, I don't need to sort everything
 numbers = [8, 2, 4, 9, 6, 10]
 large = numbers[0]
-
+second = float("-inf")
 for num in numbers:
-    if num> large:
+    if num > large:
+        second = large
         large = num
+    elif large > num > second :
+        second = num
 
 print(f"Largest number = {large}")
+print(f"Second largest number = {second}")
