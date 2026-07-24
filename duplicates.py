3b7448f4f781473e8641acc109dd826f069cb038
sreenanda <sreenandavh@gmail.com>
2026-07-24 12:17:40 +0530

    Second session

--- duplicates.py
+++ duplicates.py
@@ -1,7 +1,10 @@
 old_list = [1,2,2,3,4,4,5]
 new_list = []
+seen = set()
+
 for number in old_list:
-    if number not in new_list:
+    if number not in seen:
+        seen.add(number)
         new_list.append(number)
 
 print(new_list)
