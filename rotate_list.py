07e83f7b02c73126c59841eb4b5ba8ce12405acb
sreenanda <sreenandavh@gmail.com>
2026-07-25 09:03:57 +0530

    seventh method

--- rotate_list.py
+++ rotate_list.py
@@ -1,8 +1,6 @@
 num1 = [1,2,3,4,5]
-number = num1[-1]
-num2 = []
-num2.append(number)
-for i in num1[:-1]:
-    num2.append(i)
-print(num2)
+
+rotated = [num1[-1]] + num1[:-1]
+
+print(rotated)
     
