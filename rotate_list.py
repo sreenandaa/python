ff997ac2579d17486822111dbed43eb41f403789
sreenanda <sreenandavh@gmail.com>
2026-07-25 08:16:44 +0530

    Fourth method

--- rotate_list.py
+++ rotate_list.py
@@ -1,5 +1,5 @@
 num1 = [1,2,3,4,5]
-num2 = num1[::-1]
-# since there are no start and stop and there is a negative step,
-# Python starts from the last
+num2 = list(reversed(num1))
+#reversed function creates a reverse iterator object only. so we gotta convert it to a list
+
 print(num2)
