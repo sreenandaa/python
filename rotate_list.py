60814d48c4df8980cd98b96ec153a0b0fd70ce87
sreenanda <sreenandavh@gmail.com>
2026-07-25 08:12:02 +0530

    Third method

--- rotate_list.py
+++ rotate_list.py
@@ -1,7 +1,5 @@
 num1 = [1,2,3,4,5]
-num2 = []
-
-for i in range(len(num1)-1,-1,-1):
-    num2.append(num1[i])
-    
+num2 = num1[::-1]
+# since there are no start and stop and there is a negative step,
+# Python starts from the last
 print(num2)
