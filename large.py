3d2c6621d22f17d9de0a4cd647cbcb5409c2e674
sreenanda <sreenandavh@gmail.com>
2026-07-25 07:45:08 +0530

    First method

--- large.py
+++ large.py
@@ -1,12 +1,18 @@
 # sorting a set will return a list
-#if I want the sec largest number, I don't need to sort everything
+# If I want the second-largest number, I don't need to sort everything
 numbers = [8, 2, 4, 9, 6, 10]
 large = numbers[0]
 second = float("-inf")
+
+# I am not using any values from the list because the list might be empty
+#if I don't initialize them first, I can't use them in comparison
+
 for num in numbers:
     if num > large:
         second = large
         large = num
+#if the number is less than large, but still there is a chance the number might be greater than the present large
+    elif large > num > second :
     elif large > num > second :
         second = num
 
