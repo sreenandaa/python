3e68659103b008a2a9135fc1705a8fd181bc7895
sreenanda <sreenandavh@gmail.com>
2026-07-25 08:18:59 +0530

    Fifth method

--- rotate_list.py
+++ rotate_list.py
@@ -1,5 +1,3 @@
 num1 = [1,2,3,4,5]
-num2 = list(reversed(num1))
-#reversed function creates a reverse iterator object only. so we gotta convert it to a list
-
-print(num2)
+num1.reverse()
+print(num1)
