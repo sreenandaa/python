75191bad223882f77c68c68739f9fae420e557be
sreenanda <sreenandavh@gmail.com>
2026-07-25 08:06:26 +0530

    Second method

--- rotate_list.py
+++ rotate_list.py
@@ -1,9 +1,7 @@
 num1 = [1,2,3,4,5]
 num2 = []
-i = -1
-while abs(i) <= len(num1):
+
+for i in range(len(num1)-1,-1,-1):
     num2.append(num1[i])
-    if i == -(len(num1)):
-        break
-    i = i - 1
+    
 print(num2)
