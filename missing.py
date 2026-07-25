af6765f08094fdcdae98e23410aba9d2e0fcf1b1
sreenanda <sreenandavh@gmail.com>
2026-07-25 12:23:58 +0530

    Missing 2

--- missing.py
+++ missing.py
@@ -2,12 +2,11 @@ numbers = [2, 3, 5, 7, 9]
 
 i = 1
 j=0
-while True:
+while j < len(numbers):
     if i != numbers[j]:
-        print(f"{i} is missing\n")
+        print(f"{i} is missing")
         i += 1
         continue
     i += 1
     j += 1
-    if j == len(numbers):
-        break
+   
